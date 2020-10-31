c97ca396b1da4dd4f2975402d764d56a0cd1b631
Haruna <harunanjie@hotmail.com>
2020-10-31 05:55:05 -0700

    added search to doc_list template

--- accounts/views.py
+++ accounts/views.py
@@ -10,23 +10,40 @@ class DocsPageView(TemplateView):
     template_name = "accounts/doc_list.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['docs_list'] = Doctor.objects.all()
-        doc = Doctor.objects.get(user=self.request.user)
-        context['docs_patients_list'] = doc.patient_set.all()
-        context['patients'] = Patient.objects.all()
-        return context
+        if self.request.user.is_authenticated:
+
+            context = super().get_context_data(**kwargs)
+            # context['docs_list'] = Doctor.objects.all()
+            doc = Doctor.objects.get(user=self.request.user)
+            context['docs_patients_list'] = doc.patient_set.all()
+            context['patients'] = Patient.objects.all()
+            return context
 
 class AdminHomeView(TemplateView):
     template_name = "accounts/admin_dashboard.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['docs'] = Doctor.objects.all()
-        context['patients_list'] = Patient.objects.all()
-        return context
+        if self.request.user.is_authenticated and self.request.user.is_superuser:
+            context = super().get_context_data(**kwargs)
+            context['docs'] = Doctor.objects.all()
+            context['patients_list'] = Patient.objects.all()
+            query =  self.request.GET.get('q')
+            if query:
+                context['query']= query
+                res = Doctor.objects.filter(user__first_name__icontains=query)
+                context['res']=res
+            return context
 
-# views
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        res = 'none found'
+        if query:
+            res = Doctor.objects.filter(user__first_name__icontains=query)
+            return res
+        else:
+            return 'Search not found'
+
+
 class DoctorDetailView(DetailView):
     model = Doctor
     template_name = 'accounts/doc_detail.html'
@@ -36,6 +53,7 @@ class DoctorDetailView(DetailView):
         if self.request.user.is_authenticated:
             context = super().get_context_data(**kwargs)
             doc_id = Doctor.objects.get(user=self.request.user)
+            context['doctor']= doc_id
             context['patients_list'] = doc_id.patient_set.all()
             doc_id =self.request.user.first_name
             context['ID']= doc_id
@@ -48,3 +66,4 @@ class UserSignupView(CreateView):
     template_name = 'registration/signup.html'
     context_object_name = 'form'
     success_url = reverse_lazy('login')
+
