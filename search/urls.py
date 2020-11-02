9eccb14bab0e156fc04d64dd1f708b19933ad39a
Haruna <harunanjie@hotmail.com>
2020-11-02 05:36:11 -0800

    implemented full text search

--- search/urls.py
+++ search/urls.py
@@ -4,5 +4,6 @@ from .  import views
 app_name = 'search'
 urlpatterns = [
     path('list/', views.SearchView.as_view(), name='results'),
+    path('form/', views.SearchFormView.as_view(), name = 'formView'),
 
 ]
--- search/views.py
+++ search/views.py
@@ -1,8 +1,19 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from .models import City
+from django.db.models import Q
 # Create your views here.
 
 class SearchView(ListView):
     model = City
     context_object_name = 'cities'
     template_name = 'search/cities_list.html'
+
+    def get_queryset(self):
+        name = self.request.GET.get('name')
+        region = self.request.GET.get('region')
+        results = City.objects.filter(Q(name__icontains=name)| Q(region__icontains=region))
+        return results
+
+
+class SearchFormView(TemplateView):
+    template_name = 'search/search_form.html'
